725945e3297edf95fa1384b9b00854ed7ccf4cca
miguelansaro <72105696+miguelansaro@users.noreply.github.com>
2021-02-02 15:28:32 -0500

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,15 +1,3 @@
-var yellow=document.getElementById("amarillo");
-var blue=document.getElementById("azul");
-var red=document.getElementById("rojo");
-var valor = document.getElementById("texto").value;
 function Cambiar(){
-    if(valor===1){
-        document.body.style.backgroundColor="yellow";
-       }
-       if(valor===2){
-        document.body.style.backgroundColor="blue";
-       }
-        if(valor===3){
-        document.body.style.backgroundColor="red";
-       }
+   document.body.style.backgroundColor=document.getElementById("ingreso").value;
 }
